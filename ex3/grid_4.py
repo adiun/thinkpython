a1acd3d29bb88b4f3b71c21388bcc2e72ae04274
Aditya Unnithan <adi@MacBook-Pro.local>
2016-11-02 19:53:52 -0700

    Improving perf from O(n^2) to O(n)

--- ex3/grid_4.py
+++ ex3/grid_4.py
@@ -1,4 +1,6 @@
 
+# Performance: O(n^2)
+
 def printGrid(sideLen, numBoxes):
     if sideLen <= 0: return None
     if numBoxes <= 0: return None
@@ -12,9 +14,17 @@ def printGrid(sideLen, numBoxes):
 
 def getRow(numVertices, vertexChar, numNonVertices, nonVertexChar):
     row = ''
-    for i in range(numVertices):
-        row += vertexChar 
-        if i < numVertices - 1:
-            for j in range(numNonVertices):
-                row += nonVertexChar
+    # If there are n vertices and m non-vertices then there will be n * m + 1 chars (+1 for the last vertex)
+    # n = 3, m = 2
+    # 1 2 3 4 5 6 7
+    # * - - * - - *
+    totalChars = numVertices * numNonVertices + 1
+    for i in range(totalChars):
+        if i == totalChars - 1:
+            row += vertexChar
+        elif i % numVertices == 0:
+            row += vertexChar
+        else:
+            row += nonVertexChar
+
     return row + '\n'
